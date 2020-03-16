e5fa5f7f4806a04d94e960e10c0b9dcb488f6318
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-03-16 23:10:13 +0900

    feat: add handout theme.🐥

--- src/infrastructure/library/ThemeLibrary.ts
+++ src/infrastructure/library/ThemeLibrary.ts
@@ -4,7 +4,7 @@ import shuffle from 'lodash/shuffle';
 
 @injectable()
 export class ThemeLibrary {
-  private readonly BASE_THEME = config.get<string[]>('');
+  private readonly BASE_THEME = config.get<string[]>('theme.default');
 
   getTheme(): string {
     return shuffle(this.BASE_THEME)[0];
