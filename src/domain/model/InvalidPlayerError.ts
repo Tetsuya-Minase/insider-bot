7b5c2fea0f767b8b0b38007b957ed2dbb3f9ccf8
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-11-24 12:10:35 +0900

    refactor: fix error message.🔧

--- src/domain/model/InvalidPlayerError.ts
+++ src/domain/model/InvalidPlayerError.ts
@@ -2,7 +2,7 @@ export class InvalidPlayerError extends Error {
   private readonly _message: string;
   constructor(count: number) {
     super();
-    this._message = `playerは4人以上必要です。(指定されたのは${count}人です)`;
+    this._message = `プレイヤーは4人以上必要だよ。(指定されたのは${count}人だよ。)`;
   }
 
   get message() {
